298270ed58542a264c1c9e4bd75023d3208bc158
Jinkyum Park <me@jinkyumpark.com>
2024-12-21 02:45:23 +0900

    modify `start.js` to handle cases where DOM won't fully load

--- src/content/start.js
+++ src/content/start.js
@@ -1,6 +1,18 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 window.onload = async () => {
-    const button = document.querySelector('div.gb_Jc[role="button"]')
-    if (button) {
-        button.click()
-    }
+    let attempts = 0
+    const maxAttempts = 10
+
+    do {
+        await sleep(1000)
+
+        const button = document.querySelector('div.gb_Jc[role="button"]')
+        if (button) {
+            button.click()
+            return
+        }
+    } while (attempts++ < maxAttempts)
 }
